20e8a43f36c901f7be057f340f0e6a94897006c8
Jaymin West <jayminwest@gmail.com>
2022-09-14 15:58:38 -0700

    intiial

---
+++ assignment2.py
@@ -0,0 +1,34 @@
+import sys
+
+def open_infile():
+    print("Here")
+    while(True):
+        filename = input("Enter file name: ")
+        # Trying to open file:
+        try:
+            filename = open(filename, 'r')
+            break
+        except:
+            print("File doesn't exist! Try again")
+    return filename
+
+def read_file(infile):
+    mode = input("Read as string or line (S/L)? : ")
+    if (mode == 'S'):
+        string = infile.read()
+    elif(mode == 'L'):
+        text = ""
+        for curr_line in infile:
+            text += curr_line.rstrip('/n')
+    else: 
+        print("Invalid Entry")
+        sys.exit()
+    infile.close()
+
+def main():
+    open_infile
+
+
+
+if __name__ == "__main__":
+    main()
